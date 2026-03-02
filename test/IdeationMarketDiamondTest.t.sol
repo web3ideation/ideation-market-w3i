b0440b63c04c81e480d813b796327e090f01bad0
WolfgangNI <wolfgang.NFTideation@gmail.com>
2026-03-01 18:52:13 -0600

    manual review and adaption of testscripts

--- test/IdeationMarketDiamondTest.t.sol
+++ test/IdeationMarketDiamondTest.t.sol
@@ -42,21 +42,6 @@ contract IdeationMarketDiamondTest is MarketTestBase {
     /// ERC1155 purchase-time quantity rules
     /// -----------------------------------------------------------------------
 
-    // purchase fails if approval revoked between listing and purchase
-    function testPurchaseRevertsIfApprovalRevokedBeforeBuy() public {
-        uint128 id = _createListingERC721(false, new address[](0));
-
-        // Revoke marketplace approval
-        vm.prank(seller);
-        erc721.approve(address(0), 1);
-
-        vm.deal(buyer, 1 ether);
-        vm.startPrank(buyer);
-        vm.expectRevert(IdeationMarket__NotApprovedForMarketplace.selector);
-        market.purchaseListing{value: 1 ether}(id, 1 ether, address(0), 0, address(0), 0, 0, 0, address(0));
-        vm.stopPrank();
-    }
-
     // update keeps same listingId even with other activity in between
     function testUpdateKeepsListingId() public {
         _whitelistCollectionAndApproveERC721();
--- test/MarketplaceAuthorizationTest.t.sol
+++ test/MarketplaceAuthorizationTest.t.sol
@@ -7,4 +7,19 @@ import "./MarketTestBase.t.sol";
  * @title MarketplaceAuthorizationTest
  * @notice Authorization and approval invariants across create/update/purchase/cancel flows.
  */
-contract MarketplaceAuthorizationTest is MarketTestBase {}
+contract MarketplaceAuthorizationTest is MarketTestBase {
+    // purchase fails if approval revoked between listing and purchase
+    function testPurchaseRevertsIfApprovalRevokedBeforeBuy() public {
+        uint128 id = _createListingERC721(false, new address[](0));
+
+        // Revoke marketplace approval
+        vm.prank(seller);
+        erc721.approve(address(0), 1);
+
+        vm.deal(buyer, 1 ether);
+        vm.startPrank(buyer);
+        vm.expectRevert(IdeationMarket__NotApprovedForMarketplace.selector);
+        market.purchaseListing{value: 1 ether}(id, 1 ether, address(0), 0, address(0), 0, 0, 0, address(0));
+        vm.stopPrank();
+    }
+}
